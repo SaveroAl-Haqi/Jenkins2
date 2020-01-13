df532b7c1e88aea0e261632252a0775db17d3144
SaveroAl-Haqi <yusaalhaqi22@gmail.com>
2020-01-13 17:03:58 +0700

    second commit

--- Null.java
+++ Null.java
@@ -1,7 +1,7 @@
 public class Null {
 	public static void main(String args[]){
 		System.out.println("Hello World");
-		for(int i=0; i>100; i++){
+		for(int i=0; i<10; i++){
 		System.out.println(i);
 		}
 	}
